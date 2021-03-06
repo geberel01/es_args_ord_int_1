b9a00b68cf13b0ec18c9afedcf3544d9136d9a89
Gabriele Barberis Web <alertime1wp@gmail.com>
2021-03-06 08:23:02 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,15 @@
+//ESERCIZIO
+//INSERIRE DA LINEA DI COMANDO UNA STRINGA DI NUMERI SENZA SPAZI
+//ORDINARE I NUMERI IN UN ARRAY E STAMPARLI
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  int i=0;
+
+  printf("numero elementi %d\n",argc);
+  for(i=0;i<argc;i++)
+    printf("n. %d\tval: %s\t\n",i,argv[i]);
+}
